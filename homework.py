779a8094cab05acfe95a3ca4c7c959258f224c62
Nikita Ustiuzhanin <zaebumba@gmail.com>
2020-06-25 11:40:39 +0700

    Fixed

--- homework.py
+++ homework.py
@@ -26,7 +26,8 @@ class Calculator:
     def get_week_stats(self):
         date = dt.date.today()
         date_week_ago = dt.date.today() - dt.timedelta(days=7)
-        return sum([i.amount for i in self.records if date_week_ago <= i.date <= date])
+        return sum([i.amount for i in self.records
+                    if date_week_ago <= i.date <= date])
 
     def remaining(self):
         remaining = self.limit - self.get_today_stats()
@@ -36,27 +37,29 @@ class Calculator:
 class CashCalculator(Calculator):
     USD_RATE = 68.69
     EURO_RATE = 77.83
-        
+
     def get_today_cash_remained(self, currency):
-        if (self.remaining()) == 0:
+        remains = self.remaining()
+
+        if remains == 0:
             return f'Денег нет, держись'
 
         result = {
-            'rub': str(abs(self.remaining())) + ' руб',
-            'usd': str(abs(round((self.remaining())/self.USD_RATE, 2))) + ' USD',
-            'eur': str(abs(round((self.remaining())/self.EURO_RATE, 2))) + ' Euro'
+            'rub': str(abs(remains)) + ' руб',
+            'usd': str(abs(round(remains/self.USD_RATE, 2))) + ' USD',
+            'eur': str(abs(round(remains/self.EURO_RATE, 2))) + ' Euro'
         }
-        
-        if (self.remaining()) > 0:
+
+        if remains > 0:
             return f'На сегодня осталось {result[currency]}'
-        
+
         return f'Денег нет, держись: твой долг - {result[currency]}'
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
         if self.remaining() > 0:
-            return f'Сегодня можно съесть что-нибудь ещё,' + \
-                   f' но с общей калорийностью не более {self.remaining()} кКал'
-        
+            return f'Сегодня можно съесть что-нибудь ещё, ' + \
+                   f'но с общей калорийностью не более {self.remaining()} кКал'
+
         return 'Хватит есть!'
